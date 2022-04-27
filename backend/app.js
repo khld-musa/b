fdda4483a16b7e79b064eb6dc52bec9c01c160dd
khld-musa <elex22.sust@gmail.com>
2022-04-27 15:35:38 -0700

    full-project

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const https = require("https");
 
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
@@ -14,12 +13,11 @@ const errorMiddleware = require('./middlewares/errors')
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 // dotenv.config({ path: 'backend/config/config.env' })
 
-
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(fileUpload());
-app.enable('trust proxy')
+
 
 // Import all routes
 const products = require('./routes/product');
