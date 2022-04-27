90a4ed89b459e21ab57d66f3a84597dd2815689d
khld-musa <elex22.sust@gmail.com>
2022-04-27 14:31:30 -0700

    full-project

--- backend/app.js
+++ backend/app.js
@@ -4,20 +4,29 @@ const app = express();
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const path = require('path')
 
 const errorMiddleware = require('./middlewares/errors')
 
 // Setting up config file 
-if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
-// dotenv.config({ path: 'backend/config/config.env' })
+// if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
+dotenv.config({ path: 'backend/config/config.env' })
+
+app.use(function(request, response, next) {
+
+    if (process.env.NODE_ENV === 'PRODUCTION' && !request.secure) {
+       return response.redirect("https://" + request.headers.host + request.url);
+    }
+
+    next();
+})
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(fileUpload());
-
+app.enable('trust proxy')
 
 // Import all routes
 const products = require('./routes/product');
