83b8d8313f24267035756aac3701501c726f9bae
khld-musa <elex22.sust@gmail.com>
2022-04-28 19:32:23 -0700

    first commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,30 +8,31 @@ const cloudinary = require('cloudinary')
 // Create new product   =>   /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
-    let images = []
-    if (typeof req.body.images === 'string') {
-        images.push(req.body.images)
-    } else {
-        images = req.body.images
-    }
+    // let images = []
+    // if (typeof req.body.images === 'string') {
+    //     images.push(req.body.images)
+    // } else {
+    //     images = req.body.images
+    // }
 
-    let imagesLinks = [];
+    // let imagesLinks = [];
 
-    for (let i = 0; i < images.length; i++) {
-        const result = await cloudinary.v2.uploader.upload(images[i], {
-            folder: 'products'
-        });
+    // for (let i = 0; i < images.length; i++) {
+    //     const result = await cloudinary.v2.uploader.upload(images[i], {
+    //         folder: 'products'
+    //     });
 
-        imagesLinks.push({
-            public_id: result.public_id,
-            url: result.secure_url
-        })
-    }
+    //     imagesLinks.push({
+    //         public_id: result.public_id,
+    //         url: result.secure_url
+    //     })
+    // }
 
-    req.body.images = imagesLinks
-    req.body.user = req.user.id;
+    // req.body.images = imagesLinks
+    // req.body.user = req.user.id;
 
-    const product = await Product.save(req.body);
+
+    const product = await Product.create(req.body);
 
     res.status(201).json({
         success: true,
