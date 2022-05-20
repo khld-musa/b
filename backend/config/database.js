9c1a2744c42737b5d02b05e550f5bbd68ebcda17
khld-musa <elex22.sust@gmail.com>
2022-05-19 23:55:07 -0700

    product route update

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,23 +1,13 @@
 const mongoose = require("mongoose");
 
-// const connectDatabase = () => {
-//     mongoose.connect(process.env.DB_URI, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         useCreateIndex: true
-//     }).then(con => {
-//         console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
-//     })
-// }
 const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
+    mongoose.connect(process.env.DB_LOCAL_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }).then(con => {
+        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
     })
-    .then(() => console.log(`MongoDB Database connected with HOST: ${con.connection.host}`))
-    .catch((err) => console.log(err));
-};
+}
+
 module.exports = connectDatabase;
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,15 +11,23 @@ const cloudinary = require('cloudinary');
 // Register a user   => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
-    // const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
-    //     folder: 'avatars',
-    //     width: 150,
-    //     crop: "scale"
-    // })
+    const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: 'avatars',
+        width: 150,
+        crop: "scale"
+    })
 
-    // const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-    const user = await User.create(req.body)
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: result.public_id,
+            url: result.secure_url
+        }
+    })
 
     sendToken(user, 200, res)
 
